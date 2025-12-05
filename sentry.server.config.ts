af06f336a54a06c2ec9a22aacb79fbc358bb2327
KreoSteel <tatarchukluca@gmail.com>
2025-12-05 15:08:54 +0100

    feat: integrate Sentry for error tracking and update configuration
    - Add Sentry for improved error monitoring and reporting in the application.
    - Update Next.js configuration to include Sentry settings and enhance source map uploads.
    - Modify .gitignore to include Sentry configuration files and ensure proper file management.

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a73ba37baa2bc9eb9a477303c5bde0c7@o4510478550499328.ingest.de.sentry.io/4510478552334416",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
